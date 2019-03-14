37267a4a01967fbf7978de4dc5ad250a0f5c1515
aashishbiradar <aashish.biradar@gmail.com>
2019-03-14 12:59:30 +0530

    Added getGrades

--- app-promises.js
+++ app-promises.js
@@ -8,7 +8,19 @@ const users = [{
     schoolId : 999
 }];
 
-const grades = [{}];
+const grades = [{
+    id : 1,
+    schoolId : 111,
+    grade : 96
+},{
+    id : 2,
+    schoolId : 999,
+    grade : 92
+},{
+    id : 3,
+    schoolId : 111,
+    grade : 89
+}];
 
 const getUser = (id) => {
     return  new Promise ((resolve,reject)=>{
@@ -21,8 +33,20 @@ const getUser = (id) => {
     });
 };
 
+const getGrades = (schoolId) => {
+    return new Promise ((resolve,reject) => {
+        resolve(grades.filter((grade) => grade.schoolId === schoolId));
+    })
+};
+
 getUser(2).then((user) => {
     console.log(user);
+}).catch((e) => {
+    console.log(e);
+});
+
+getGrades(111).then((grades) => {
+    console.log(grades);
 }).catch((e) => {
     console.log(e);
 });
